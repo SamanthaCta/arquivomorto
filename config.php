f69944c25a96997d541cd56d2103ca4bd833fee1
Samantha <sc147099@gmail.com>
2022-09-02 19:31:54 -0300

    first commit

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php 
+$banco = "arquivo_morto"; 
+$usuario = "root"; 
+$senha = "";
+$host = "localhost"; 
+
+global $db; 
+try { 
+    $db = new PDO( "mysql:host=" . $host. ";dbname=". $banco, $usuario , $senha); 
+}catch ( PDOException $e ){ 
+    echo $e -> getMessage();
+}
